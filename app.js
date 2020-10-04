5e66d94edb127437f684c73585a41413036be0f9
jiaenchua <jiaen_je@hotmail.com>
2020-10-04 16:06:06 -0500

    chg mlab db url to mongo atlas

--- app.js
+++ app.js
@@ -16,34 +16,36 @@ var indexRoutes = require("./routes/index");
 
 //seedDB();
 // mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://project:review@ds245548.mlab.com:45548/projectreview");
+mongoose.connect(process.env.DATABASEURL);
 
-app.use(bodyparser.urlencoded({extended:true}));   
+app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 //PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
 });
 
 app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function (){
-    console.log("Yelpcamp Server has started!");
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("Yelpcamp Server has started!");
 });
